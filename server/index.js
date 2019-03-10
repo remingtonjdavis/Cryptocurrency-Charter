f5cf20af3f2907b948970ae03c288d5d098bce6d
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-03-10 08:01:47 -0500

    Created Basic ReactApp Filestructure

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/../public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(port, () => console.log('Now listening on port: ' + port));
